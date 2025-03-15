ae05c7aa3861e49c6a9b27851d7b502b6138ae37
muhammadakbarra <13120210008@umi.ac.id>
2025-03-16 05:21:18 +0800

    fix: cross origin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(helmet());
+app.use(
+    helmet({
+        crossOriginResourcePolicy: { policy: 'cross-origin' },
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
